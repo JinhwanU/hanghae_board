74561ec6f5f0f4523b3d472b7a78e1d112ee3d59
JinhwanU <jhwan211@naver.com>
2022-02-16 20:29:43 +0900

    완성

--- src/main/java/com/sparta/board/controller/CommentRestController.java
+++ src/main/java/com/sparta/board/controller/CommentRestController.java
@@ -15,17 +15,10 @@ public class CommentRestController {
     private final CommentRepository commentRepository;
     private final CommentService commentService;
 
-//    @GetMapping("api/posts/{id}/comments")
     @GetMapping("api/comments")
     public List<Comment> getComments() {
         return commentRepository.findAllByOrderByCreatedAtDesc();
     }
-
-//    @PostMapping("api/posts/{id}/comments")
-//    public Comment createComment(@RequestBody CommentRequestDto commentRequestDto) {
-//        Comment comment = new Comment(commentRequestDto);
-//        return commentRepository.save(comment);
-//    }
 
     @PostMapping("api/{id}/comments")
     public ResponseEntity<Success> createComment(@PathVariable Long id, @RequestBody CommentRequestDto requestDto) {
@@ -33,14 +26,12 @@ public class CommentRestController {
         return new ResponseEntity<>(new Success(true, "댓글 등록 완료"), HttpStatus.OK);
     }
 
-//    @DeleteMapping("api/posts/{postId}/comments/{commentId}")
     @DeleteMapping("api/comments/{id}")
     public Long deleteComment(@PathVariable Long id) {
         commentRepository.deleteById(id);
         return id;
     }
 
-//    @PutMapping("api/posts/{postId}/comments/{commentId}")
     @PutMapping("api/comments/{id}")
     public Long updateComment(@PathVariable Long id, @RequestBody CommentRequestDto requestDto) {
         return commentService.updateComment(id, requestDto);
